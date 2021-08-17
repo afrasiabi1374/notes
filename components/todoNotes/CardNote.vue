e90947cd67a7408ed64625bd45814f02a718d91b
afrasiabi1374 <mohammad.aligiga@gmail.com>
2021-08-17 17:53:19 +0430

    ali commit

--- components/todoNotes/CardNote.vue
+++ components/todoNotes/CardNote.vue
@@ -3,11 +3,21 @@
     <h2> {{note.title}} </h2>
     <br>
     <p> {{  note.text.substr(0,21)+'...'}} </p>
+    <div class="container">
+        <v-chip
+          v-for="(tag,tagIndex) in tags" :key="tagIndex"
+          class="ma-2"
+          x-small>
+          
+
+          {{tag}}
+        </v-chip>
+    </div>
   </v-card>
 </template>
 <script>
   export default {
-  props: ['note']
+  props: ['note','tags']
   }
 </script>
 <style >
@@ -20,6 +30,9 @@
       background-color: rgba(32, 178, 171, 0.493);
       transition: all 1s;
       cursor: pointer;
+  }
+  .container{
+    display: flex;
   }
 </style>
 
--- pages/index.vue
+++ pages/index.vue
@@ -6,14 +6,13 @@
         <v-row no-gutters>
           <v-col cols="12" sm="4"
             v-for="(note, index) in allNotes" :key="index" >
-            <card-note :note="note" @click="formNote=note" v-on:changeTags="showTag = $event;logger()" />
-            
+            <card-note :note="note" @click="formNote=note" :tags="showTag" /> 
           </v-col>
         </v-row>
         <v-btn  @click="formNote={title: '', text: ''};">new note</v-btn>
       </v-container >
       <!-- <note-form v-model="formNopte" :value="formNote" @input="(v) => formNote=v" /> -->
-      <note-form v-model="formNote" />
+      <note-form v-model="formNote" v-on:changeTags="showTag = $event;logger()"/>
     </div>
   </div>
 </template>
